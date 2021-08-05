51da6d9f3e2b7c632b80ed3603107ac81f423f18
happyislandR <masahiro.fukushima@rakus-partners.co.jp>
2021-08-05 15:13:00 +0900

    AdministratorServiceクラスの修正
     - insertメソッドの追加

--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -4,11 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.repository.AdministratorRepository;
 
+/**
+ * 管理者情報をリポジトリに受け渡しをするサービス
+ * 
+ * @author fukushima
+ *
+ */
 @Service
 @Transactional
 public class AdministratorService {
 	@Autowired
 	private AdministratorRepository administratorRepository;
+	
+	/**
+	 * 管理者情報を挿入する.
+	 * 
+	 * @param administrator 管理者情報
+	 */
+	public void insert(Administrator administrator) {
+		administratorRepository.insert(administrator);
+	}
 }
