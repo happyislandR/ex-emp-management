c211a109f320a2b6ac8ed4e06e2e5fad6d0f3267
happyislandR <masahiro.fukushima@rakus-partners.co.jp>
2021-08-06 11:01:13 +0900

    EmployeeServiceクラスの定義

---
+++ src/main/java/jp/co/sample/service/EmployeeService.java
@@ -0,0 +1,42 @@
+package jp.co.sample.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jp.co.sample.domain.Employee;
+import jp.co.sample.repository.EmployeeRepository;
+
+/**
+ * 従業員情報一覧をリポジトリに受け渡しをするサービス.
+ * 
+ * @author fukushima
+ *
+ */
+@Service
+@Transactional
+public class EmployeeService {
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	/**
+	 * 従業員情報を全件取得する.
+	 * 
+	 * @return 従業員情報
+	 */
+	public List<Employee> showList() {
+		return employeeRepository.findAll();
+	}
+	
+	/**
+	 * 従業員情報を取得する.
+	 * 
+	 * @param id 従業員ID
+	 * @return 従業員情報
+	 */
+	public Employee showDetail(Integer id) {
+		return employeeRepository.load(id);
+	}
+}
